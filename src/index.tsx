307523151915165adcecaa5b33cf6660a090f754
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2021-12-06 01:36:37 +0200

    attempt to get service worker running

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+// eslint-disable-next-line import/no-unresolved
+import { registerSW } from 'virtual:pwa-register';
 
 import { ThemeProvider, useTheme } from '@mui/material';
 
 import { store } from './store/store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// import * as serviceWorker from './serviceWorker';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
+
+const intervalMS = 60 * 60 * 1000;
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+registerSW({
+    onRegistered(r) {
+        if (!r) return;
+        setInterval(() => {
+            r.update();
+        }, intervalMS);
+    },
+});
 
 const ProviderComponent: React.FC = () => {
     const theme = useTheme();
@@ -28,4 +45,4 @@ ReactDOM.render(<ProviderComponent />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,39 @@ import { resolve } from 'path';
 
 import { UserConfig } from 'vite';
 import Checker from 'vite-plugin-checker';
+import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import react from '@vitejs/plugin-react';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+    mode: 'development',
+    // includeAssets: ['favicon.svg'],
+    includeAssets: ['camera.ico', 'robots.txt'],
+    manifest: {
+        name: 'Gopro web-bluetooth remote',
+        short_name: 'Gopro web-ble remote',
+        description: 'Description of your app',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'camera.ico',
+                sizes: '64x64 32x32 24x24 16x16',
+                type: 'image/x-icon',
+            },
+            {
+                src: 'camera128.png',
+                type: 'image/png',
+                sizes: '128x128',
+            },
+            {
+                src: 'camera512.png',
+                type: 'image/png',
+                sizes: '512x512',
+            },
+        ],
+    },
+};
 
 function pathResolve(dir: string) {
     return resolve(__dirname, '.', dir);
@@ -16,6 +46,9 @@ const config: () => UserConfig = () => ({
     server: {
         hmr: { port: 443 },
     },
+    build: {
+        sourcemap: true,
+    },
     resolve: {
         alias: [
             {
@@ -25,6 +58,7 @@ const config: () => UserConfig = () => ({
         ],
     },
     plugins: [
+        VitePWA(pwaOptions),
         tsconfigPaths(),
         react({
             babel: {
