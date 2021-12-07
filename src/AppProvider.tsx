dc06e83ddde444da7cba72639208ea0691366763
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2021-12-07 21:01:44 +0200

    Remove body margin

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,6 +1,7 @@
 import { ServiceWorkerContainer } from 'components/pwa/ServiceWorkerContainer';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { GlobalStyles } from 'tss-react';
 
 import { ThemeProvider, useTheme } from '@mui/material';
 
@@ -13,6 +14,14 @@ export const AppProvider: React.FC = () => {
     const theme = useTheme();
     return (
         <Provider store={store}>
+            <GlobalStyles
+                styles={{
+                    body: {
+                        margin: 0,
+                        padding: 0,
+                    },
+                }}
+            />
             <ThemeProvider theme={theme}>
                 <ServiceWorkerContainer />
                 <ToastContainer />
