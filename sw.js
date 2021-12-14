44d8dd737773a15ae1b7d74823c56243e230e376
Woyken <Woyken@users.noreply.github.com>
2021-12-14 00:13:15 +0000

    deploy: 7f76fde59742ee8247a7d93cd564ff2c1d67ac8f

--- sw.js
+++ sw.js
@@ -93,20 +93,20 @@ define(['./workbox-a51b997a'], (function (workbox) { 'use strict';
    */
 
   workbox.precacheAndRoute([{
-    "url": "assets/index.295e7a2c.js",
-    "revision": null
-  }, {
     "url": "assets/index.5561b2f0.css",
     "revision": null
   }, {
-    "url": "assets/react.2392e46c.js",
+    "url": "assets/index.fb095d72.js",
     "revision": null
   }, {
-    "url": "assets/vendor.d41964c3.js",
+    "url": "assets/react.b00e5675.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor.e945af34.js",
     "revision": null
   }, {
     "url": "index.html",
-    "revision": "4d14281d8754d41471edd0d3b9bcbe97"
+    "revision": "da935cee30a382d658e2f3427f972341"
   }, {
     "url": "camera.ico",
     "revision": "043eb22b6e8605bbceb61cf021c602a8"
